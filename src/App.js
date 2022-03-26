d4ea174430c138d8325717c46ef719fc2be18772
ssalvi8586 <ssalvi8586@gmail.com>
2022-03-26 12:39:30 +0600

    json added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Grid, Typography } from "@mui/material"
+import Watches from './components/Watches/Watches';
+import Cart from './components/Cart/Cart';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography
+        style={{ fontWeight: 400 }}
+        align="center"
+        m={2}
+        variant="h2"
+        component="h2"
+        color="secondary">
+        Get a watch
+      </Typography>
+      <Typography
+        align="center"
+        variant="h4"
+        component="h4">
+        Choose upto 4
+      </Typography>
+      <Grid container spacing={2} m={2} p={2}>
+        <Grid item xs={12} md={6} lg={8}>
+          <Watches></Watches>
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <Cart></Cart>
+        </Grid>
+      </Grid>
+
     </div>
   );
 }
---
+++ src/components/Watches/Watches.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const Watches = () => {
+    return (
+        <div>
+            const [first, setfirst] = useState(second);
+        </div>
+    );
+};
+
+export default Watches;
